9304c49fdae34f0b2f2cbcf47c701b7a3f98114d
Isa Mammadli <i.a.memmedli@gmail.com>
2025-10-01 16:16:41 +0400

    Pagetitle responsibility was ready

--- frontend/src/components/PageTitle.jsx
+++ frontend/src/components/PageTitle.jsx
@@ -5,7 +5,12 @@ const PageTitle = ({ title, customClass = "" }) => {
     return (
         <div className={`page-title-container ${customClass}`}>
             <div className="page-title-content">
-                <h1 className="page-title">{title}</h1>
+                <h1 style={{ textTransform: "uppercase", fontSize: "5.8rem",
+                    background: "linear-gradient(90deg, #17DBFC 0%, #FFFFFF 50%, #467EFE 100%)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent"
+                }} >SOFTECH</h1>
+                <h2 className="page-title">{title}</h2>
             </div>
         </div>
     );
